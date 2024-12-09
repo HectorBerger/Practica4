4b35e3173eded3041e07f35379868ca753ddc562
HectorBerger <hectorbergermartinez@gmail.com>
2024-12-09 09:47:02 +0100

    create file

--- my_tee.c
+++ my_tee.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
-#include <uistd.h>
+#include <unistd.h>
 
-int main ( int argc, char *argv[] )
+
+int main ( int argc, char *argv[] ){
+	int fd;
+	//char filename=strcat("../"+argv[0]+"/";
+	if (argc>1){
+		fd=open(argv[0], O_RDWR | O_CREAT, 0644);
+		if (fd==-1){
+			perror("[-]");
+			exit;
+		}
+	}
+	close(fd)
+}
