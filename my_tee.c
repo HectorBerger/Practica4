ca889777e9f510015eef74682a4c85a6d4fe1c86
HectorBerger <hectorbergermartinez@gmail.com>
2024-12-16 09:09:58 +0100

    No da errores pero no funciona

--- my_tee.c
+++ my_tee.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+//#include <stdin.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -13,5 +14,18 @@ int main ( int argc, char *argv[] ){
 			exit(-1);
 		}
 		close(fd);
+	}
+	else{
+		char buf[1];
+        	ssize_t lectura;
+        	ssize_t a;
+		ssize_t b;
+		int count;
+		lectura = read(0, buf, 1);
+        	while (lectura!=-1){
+                	a = write(1, buf, count);
+                        b = write(fd, buf, count);
+                        lectura = read(0, buf, 1);
+		}
 	}
 }
