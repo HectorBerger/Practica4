39d9b68ebadbf031308897771e8f5ebad1100ce7
HectorBerger <hectorbergermartinez@gmail.com>
2024-12-19 13:27:47 +0100

    My_cmp funcional

--- my_cmp.c
+++ my_cmp.c
@@ -6,7 +6,10 @@
 int main ( int argc, char *argv[] ){
         int fd1;
 	int fd2;
-        if (argc>2){
+	if (argc!=3){
+		printf("Ús: %s fitxer1 fitxer2\n", argv[0]);
+       		exit(-1);
+        }else{
                 fd1 = open(argv[1], O_RDONLY | O_CREAT, 0644);
                 if (fd1==-1){
                         perror("[-]");
@@ -20,20 +23,22 @@ int main ( int argc, char *argv[] ){
                         exit(-1);
                 }
 		ssize_t	a,b;
-		char buf1,buf2;
+		char buf1[1],buf2[1];
 		int byte=1,linia=1;
 		a=read(fd1,buf1,1);
 		b=read(fd2,buf2,1);
-                if ( || (a!=0 && b!=0)){
+                while ((buf1[0]==buf2[0]) && (a!=0 && b!=0)){
 			byte++;
+			if(buf1[0] == '\n'){
+                        	linia++;
+			}
 			a=read(fd1,buf1,1);
                 	b=read(fd2,buf2,1);
-			if(buf=="\n"){
-				linia++;
-			}
+		}
+		if (!(a==0 && b==0)){
+			printf("%s %s differ: byte %d, line %d\n",argv[1],argv[2],byte,linia);
+		}
 		close(fd1);
 		close(fd2);
-		printf("%c %c differ: byte %d, line %d\n",argv[0],argv[1],byte,linia);
-		}
 	}
 }
